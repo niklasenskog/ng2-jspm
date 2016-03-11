bef6afb02f39426201d0cf3245318d64acbf322d
Niklas Enskog <niklas.enskog@cgi.com>
2016-03-11 17:55:43 +0100

    Föresöker få gulp att kompilera ts

--- src/Ng2-jspm/gulpfile.js
+++ src/Ng2-jspm/gulpfile.js
@@ -1,20 +1,56 @@
-﻿/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
+﻿///*
+//This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+//Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+//*/
 
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
+//var gulp = require("gulp");
+//var ts = require("gulp-typescript");
 
-var tsProject = ts.createProject("tsconfig.json");
+//var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("scripts", function () {
-    var tsResult = tsProject.src() // instead of gulp.src(...) 
-		.pipe(ts(tsProject));
+//gulp.task("scripts", function () {
+//    var tsResult = tsProject.src() // instead of gulp.src(...) 
+//		.pipe(ts(tsProject));
 
-    return tsResult.js.pipe(gulp.dest("."));
+//    return tsResult.js.pipe(gulp.dest("."));
+//});
+
+//gulp.task("default", ["scripts"], function () {
+//    // place code for your default task here
+//});
+
+var gulp = require("gulp"),
+    ts = require("gulp-typescript"),
+    merge = require("merge"),
+    flatten = require("flatten");
+
+var webroot = "wwwroot";
+
+var paths = {
+    tsSource: "./" + webroot + "/app/**/*.ts",
+    tsOutput: "./" + webroot + "/app",
+    tsDef: "./typings/"
+};
+
+var tsCompilerConfig = ts.createProject("tsconfig.json");
+
+gulp.task("ts-compile", function () {
+    console.log("Source: " + paths.tsSource);
+    var tsResult = gulp.src(paths.tsSource)
+        .pipe(ts(tsCompilerConfig));
+
+    return merge([
+        tsResult.dts.pipe(gulp.dest(paths.tsDef)),
+        tsResult.js.pipe(gulp.dest(paths.tsOutput))
+    ]);
 });
 
-gulp.task("default", ["scripts"], function () {
-    // place code for your default task here
+gulp.task("watch", ["ts-compile"], function () {
+    gulp.watch(paths.tsSource, ["ts-compile"]);
 });
+
+//gulp.task('copy-defs', function () {
+//    gulp.src(paths.importedTypings)
+//        .pipe(flatten())
+//        .pipe(gulp.dest(paths.tsDef + "/jspmImports/"));
+//});
--- src/Ng2-jspm/wwwroot/app/main.ts
+++ src/Ng2-jspm/wwwroot/app/main.ts
@@ -1,3 +1,4 @@
 ﻿//Problem att få typescript att kompilera on build. dags för gulp: http://weblogs.asp.net/dwahlin/creating-a-typescript-workflow-with-gulp
 
-alert("Yo!");
+    alert("Yo-hoo!");
+
