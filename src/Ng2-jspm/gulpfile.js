b847b815b29d2fdeedf8bae37f45eaf7402a0ae7
Niklas Enskog <niklasenskog@gmail.com>
2016-03-11 16:19:57 +0100

    Work in progress, current snapshot

---
+++ src/Ng2-jspm/gulpfile.js
@@ -0,0 +1,20 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("scripts", function () {
+    var tsResult = tsProject.src() // instead of gulp.src(...) 
+		.pipe(ts(tsProject));
+
+    return tsResult.js.pipe(gulp.dest("."));
+});
+
+gulp.task("default", ["scripts"], function () {
+    // place code for your default task here
+});
---
+++ src/Ng2-jspm/main.js
@@ -0,0 +1,2 @@
+//Problem att få typescript att kompilera on build. dags för gulp: http://weblogs.asp.net/dwahlin/creating-a-typescript-workflow-with-gulp
+alert("Yo!");
